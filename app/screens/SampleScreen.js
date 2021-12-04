2d4b2df33fac8d0f24f2695eb83ac885b497ebba
Vihaga Yohan <vihagayohan94@gmail.com>
2021-12-04 11:50:17 +0530

    Added custom function for handle rotation

--- app/screens/SampleScreen.js
+++ app/screens/SampleScreen.js
@@ -1,9 +1,12 @@
+import {program} from '@babel/types';
 import React, {Component, useEffect} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  withRepeat,
+  withSpring,
 } from 'react-native-reanimated';
 
 const SIZE = 100;
@@ -15,14 +18,26 @@ const SampleScreen = () => {
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
       opacity: progress.value,
-      transform: [{scale: scale.value}],
+      transform: [
+        {scale: scale.value},
+        {
+          rotate: handleRotation(progress),
+        },
+      ],
     };
   }, []);
 
   useEffect(() => {
-    progress.value = withTiming(1, {duration: 5000});
-    scale.value = withTiming(2, {duration: 5000});
+    /*  progress.value = withTiming(1, {duration: 5000});
+    scale.value = withTiming(2, {duration: 5000}); */
+    progress.value = withRepeat(withSpring(0.5), 3, false);
+    scale.value = withRepeat(withSpring(1), 3, true);
   }, []);
+
+  const handleRotation = progress => {
+    'worklet';
+    return `${progress.value * 2 * Math.PI}rad`;
+  };
 
   return (
     <View style={styles.container}>
